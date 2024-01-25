37ac117136fd50026aea9fd17a9d2dfe4cf26bc4
skylsldff <144352824+skylsldff@users.noreply.github.com>
2024-01-25 16:17:05 +0100

    Update gp-setwelcome.js

--- plugins/gp-setwelcome.js
+++ plugins/gp-setwelcome.js
@@ -10,6 +10,6 @@ handler.help = ['setwelcome <text>']
 handler.tags = ['group']
 handler.command = ['setwelcome'] 
 handler.admin = true
-handler.owner = false
+handler.owner = true
 
 export default handler
